1b98f9619312521a4320ea19f80e934b521837c7
Danny Samán <68825617+dannyroot95@users.noreply.github.com>
2022-10-23 03:02:26 -0500

    Update smart_door.ino

--- smart_door.ino
+++ smart_door.ino
@@ -1,11 +1,15 @@
 #include <ESP8266WiFi.h>
-#include <FirebaseArduino.h>
+#include "FirebaseESP8266.h"
 
 #define FIREBASE_HOST "puertaelectrica-bf3d5-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "zYF4FBQeIUxx4XHMR1vpDAHbrdQvUTwIOR6fc3ui"
-#define WIFI_SSID "WRZ"
-#define WIFI_PASSWORD "Materdei20*"
-#define RELAY 4
+//#define WIFI_SSID "WRZ"
+//#define WIFI_PASSWORD "Materdei20*"
+#define WIFI_SSID "ALFREDO"
+#define WIFI_PASSWORD "@alfredoh1969"
+#define RELAY 16
+
+FirebaseData firebaseData;
 
 void setup() {
   Serial.begin(9600);
@@ -15,13 +19,14 @@ void setup() {
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    delay(500);
+    delay(5000);
   }
   Serial.println();
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
   pinMode(RELAY, OUTPUT);
 
   
@@ -31,16 +36,14 @@ void loop() {
    // get value 
    
   digitalWrite(RELAY, HIGH);
+
+  Firebase.getString(firebaseData, "status");
+  String valueData = firebaseData.stringData();
   
-  if(Firebase.getString("status") == "open")
+  if(valueData == "open")
   {
     digitalWrite(RELAY, LOW);
     delay(1000);
-  Firebase.setString("status", "closed");
-  if (Firebase.failed()) {
-      Serial.print("setting /message failed:");
-      Serial.println(Firebase.error());  
-      return;
-     }
+    Firebase.setString(firebaseData,"status","closed");
   }
 }
